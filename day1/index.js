081fcb232c5908aeb516ee4856942f6235163383
Moosees <linus.almgren@gmail.com>
2019-12-03 16:12:38 +0100

    Complete day 1

---
+++ day1/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const fuelCalculator = mass => {
+  const fuelRequired = Math.floor(mass / 3) - 2;
+  return fuelRequired > 0 ? fuelRequired + fuelCalculator(fuelRequired) : 0;
+};
+
+const countFuelForTheCounterUpper = async (input, calculator) => {
+  let counterUpper = 0;
+
+  try {
+    const fileStream = fs.createReadStream(input);
+
+    const rl = readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+
+    for await (const line of rl) {
+      counterUpper += calculator(line);
+    }
+    return counterUpper;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+countFuelForTheCounterUpper('./input.txt', fuelCalculator).then(answer =>
+  console.log('Fuel Required: ', answer)
+);
